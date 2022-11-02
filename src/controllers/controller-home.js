f23606d9b9c53572e1ce3db7895cb9958e4c9bd6
Difa <49149689+Reinlock15@users.noreply.github.com>
2022-11-02 23:02:10 +0700

    cek resi done

--- src/controllers/controller-home.js
+++ src/controllers/controller-home.js
@@ -1,23 +1,48 @@
-module.exports ={
-    home(req,res){
-        res.render("home",{
-            url: 'http://localhost:8000/',
-            userName: req.session.username,
-        });
-    },
+const config = require("../configs/database");
+const mysql = require("mysql");
+const pool = mysql.createPool(config);
 
-    homeCabang(req,res){
-        res.render("home-cabang",{
-            url: 'http://localhost:8000/',
-            userName: req.session.username,
-        });
-    },
+module.exports = {
+  home(req, res) {
+    res.render("home", {
+      url: "http://localhost:8000/",
+      userName: req.session.username,
+    });
+  },
 
-    cekResi(req,res){
-        res.render("cek-resi",{
-            url: 'http://localhost:8000/',
-            userName: req.session.username,
-        });
-    }
+  homeCabang(req, res) {
+    res.render("home-cabang", {
+      url: "http://localhost:8000/",
+      userName: req.session.username,
+    });
+  },
 
-}
+  cekResi(req, res) {
+    res.render("cek-resi", {
+      url: "http://localhost:8000/",
+      userName: req.session.username,
+    });
+  },
+
+  cekResiByResi(req, res) {
+    let sql =
+      "SELECT id_order,tabel_user.nama_user AS nama_user,tabel_user.nama_user AS list_user, table_admin.admin_name AS nama_admin, tabel_ongkir.tujuan AS tujuan, nama_barang, berat,total_harga,status,resi FROM tabel_order JOIN tabel_user ON tabel_order.id_user = tabel_user.id_user JOIN table_admin ON tabel_order.admin_id = table_admin.admin_id JOIN tabel_ongkir ON tabel_order.id_ongkir = tabel_ongkir.id_ongkir WHERE tabel_order.resi = '" +
+      req.params.resi +
+      "'";
+    let query = pool.query(sql, (err, results) => {
+      if (err) throw err;
+      res.send(results);
+      // console.log(results);
+      //   res.render("cek-resi", {
+      //     dataResi: results,
+      //     url: "http://localhost:8000/",
+      //     userName: req.session.username,
+      //   });
+
+      //   res.render("cek-resi", {
+      //     url: "http://localhost:8000/",
+      //     userName: req.session.username,
+      //   });
+    });
+  },
+};
